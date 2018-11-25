7b5be51456263b4f216a8a559d4951d404715794
Tameana Naderi <38362681+meananad@users.noreply.github.com>
2018-11-25 18:15:59 -0500

    Update RandomNumGen.java

--- RandomNumGen.java
+++ RandomNumGen.java
@@ -3,13 +3,14 @@ This program will roll dice and output 10 random results ranging from numbers 1 
 */
 
 package tameana;
-import java.util.Random;
+import java.util.Random; //import necessary extension
 
 class RandomNumGen {
 	public static void main (String[] args) {
 		Random dice = new Random();
 		int number;
 		
+		//Utilizing for loop to roll dice ten times then stop.
 		for(int counter = 1; counter <=10; counter++) {
 			number = 1+dice.nextInt(6);
 			
